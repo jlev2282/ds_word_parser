c95b44bcfa70c0343266b3e441cbae9589feccc3
John <jlev2282@gmail.com>
2017-09-13 16:48:07 -0400

    attached footer to bottom

--- parser.js
+++ parser.js
@@ -8,9 +8,10 @@ usedStrings=[];
 $("#submit").on("click", function(event){
     event.preventDefault();
     grabInput();
-
-
-    usedStrings.push(document.getElementById("stringInput").value);
+    stringArray.sort();
+    console.log(stringArray);
+    document.getElementById("alpha").innerHTML = stringArray;
+    
 });
 
 
@@ -18,6 +19,8 @@ $("#submit").on("click", function(event){
 function grabInput(){
     //grab value of input from stringInput
     string1=document.getElementById("stringInput").value;
+    usedStrings.push(string1);
+    document.getElementById("prevStrings").innerHTML = usedStrings;
     //parse string into an array of substrings eaual to stringArray
     stringArray=string1.split(' ');
     console.log(stringArray);//should show up as an array with different words in it
@@ -26,7 +29,13 @@ function grabInput(){
 
 // function that loops over array and add each unique word into a new array
 function unique(array){};
+
 // function that puts UniqueWordArray into alphabhetical order
 function alphaString(string){};
+
 // function that places current string into usedStrings array
+function prevStrings(){
+    usedStrings.push(document.getElementById("stringInput").value);
+};
+
 });
