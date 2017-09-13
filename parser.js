3c4e07de98b5abf35ffbba3c1125edf3de2d0e68
John <jlev2282@gmail.com>
2017-09-13 16:03:24 -0400

    generic push

--- parser.js
+++ parser.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
 //Define variables for arrays
 var stringArray=[];
-// stringArray2=[];
 uniqueWordArray=[];
 usedStrings=[];
 
 // Take input and enter it into an stringArray
 $("#submit").on("click", function(event){
     event.preventDefault();
+    grabInput();
 
-    //grab value of input from stringInput
-    string1=document.getElementById("stringInput").value;
-
-    //parse string into an array of substrings eaual to stringArray
-    stringArray=string1.split(' ');
-    console.log(stringArray);//should show up as an array with different words in it
 
     usedStrings.push(document.getElementById("stringInput").value);
 });
+
+
+//function that grabs input and splits into array of strings
+function grabInput(){
+    //grab value of input from stringInput
+    string1=document.getElementById("stringInput").value;
+    //parse string into an array of substrings eaual to stringArray
+    stringArray=string1.split(' ');
+    console.log(stringArray);//should show up as an array with different words in it
+    return(stringArray);
+};
 
 // function that loops over array and add each unique word into a new array
 function unique(array){};
