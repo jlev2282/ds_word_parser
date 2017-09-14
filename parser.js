d976d431d02327d8626f1353e08f68b2c8f6b94d
John <jlev2282@gmail.com>
2017-09-14 00:09:06 -0400

    first attempt to find unique words function

--- parser.js
+++ parser.js
@@ -9,6 +9,7 @@ $("#submit").on("click", function(event){
     event.preventDefault();
     grabInput();
     alphaString();
+    unique(stringArray);
     
 });
 
@@ -20,22 +21,29 @@ function grabInput(){
     document.getElementById("prevStrings").innerHTML = usedStrings;
     //parse string into an array of substrings equal to stringArray
     stringArray=string1.split(' ');
-    console.log(stringArray);//should show up as an array with different words in it
     return(stringArray);
 };
 
 // function that loops over array and add each unique word into a new array
-function unique(){
-    uniqueArray=[];
-    for (i = 0; i < stringArray.length; i++) { 
+function unique(arr){
+    var tempObj={};
+    var j = 1;
+    for (i = 0; i < arr.length; i++) {
+        tempObj[arr[i]]=j;
+        j++;
+    }
+    var uniqueArray=[];
+    for(var key in tempObj)
+    uniqueArray.push(key);
+    console.log(uniqueArray);
+    document.getElementById("uniqueCount").innerHTML = uniqueArray.length;
     };
-};
 
 // function that puts stringArray into alphabhetical order
 function alphaString(){
     stringArray.sort();
-    console.log(stringArray);
     document.getElementById("alpha").innerHTML = stringArray;
+    return(stringArray);
 };
 
 // function that places current string into usedStrings array
